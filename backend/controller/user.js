55a2a9b7e1be3b5c6663e02f0d4ad064c4733cd4
Nivesh2003 <nivesh.ch30@gmail.com>
2025-11-17 11:07:53 +0530

    login api and google sign in api route completed

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,4 +1,10 @@
 const User = require('../models/user')
+const bcryptjs = require('bcryptjs')
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+
+//User registration function (using email and password)
 exports.register = async(req,res) =>{
     try {
         console.log(req.body);
@@ -7,10 +13,58 @@ exports.register = async(req,res) =>{
         if(isUserExist){
            return res.status(400).json({error:"Already have an account with this email."})
         }
-        const newUser = new User({email,password,f_name});
+        //Secured password using bcrypt js
+        const hashedPassword = await bcryptjs.hash(password,10);
+        console.log(hashedPassword);
+        
+        const newUser = new User({email,password:hashedPassword,f_name});
         newUser.save();
         return res.status(201).json({message:"User registered successfully",success:"yes",data:User})
     } catch(err){
+        console.error(err);
+        res.status(500).json({error:"Server error"});
+    }
+}
+
+// Registration or login via Google
+exports.loginViaGoogle=async(req,res)=>{
+    try {
+        //frontend se token ayega
+        const {token} = req.body;
+        const ticket = await client.verifyIdToken({
+            idToken:token,
+            audience:process.env.GOOGLE_CLIENT_ID
+        })
+        const payload = ticket.getPayload();
+        const {sub,email,name,picture} = payload;
+        const userExists = await User.findOne({email});
+        if (!userExists) {
+            //Register new user
+            userExists = await User.create({
+                googleId:sub,
+                email,
+                f_name:name,
+                profilePic:picture
+            });
+        } 
+        return res.status(200).json({user:userExists});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error:"Server error",message:error.message})
+    }
+}
+
+//Login api
+exports.login=async(req,res)=>{
+    try{
+        let{email,password} = req.body;
+        const userExists = await User.findOne({email});
+        if(userExists && await bcryptjs.compare(password,userExists.password)){
+            return res.json({message:"User logged in successfully",success:"true",userExists})
+        } else {
+            return res.status(400).json({error:"Invalid credentials"})
+        }
+    }catch(err){
         console.error(err);
         res.status(500).json({error:"Server error"});
     }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,9 @@ const userController = require('../controller/user')
 const route = express.Router();
 
 route.post('/register',userController.register);
+route.post('/login',userController.login);
+route.post('/google',userController.loginViaGoogle);
+
 
 
 
